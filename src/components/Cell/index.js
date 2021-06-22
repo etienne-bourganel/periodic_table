d6079f855ed9045a7b5c92f85703bf0eef76a1d2
Etienne Bourganel <etienne.bourganel@gmail.com>
2021-06-22 12:12:19 +0200

    Create a new PeriodGroupCell component

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -1,42 +1,38 @@
 import React from "react"
 import "./index.css"
+import PeriodGroupCell from "../PeriodGroupCell"
 
 const Cell = ({ elementInfo, updateInfoBox }) => {
   const handleClick = () => {
     updateInfoBox(elementInfo)
-    // window.open(elementInfo.source, "_blank")
   }
 
-  // const handleMouseEnter = () => {
-  //   updateInfoBox(elementInfo)
-  // }
+  const CellIsGroupNr = () => {
+    return (
+      elementInfo.ypos === 0 && elementInfo.xpos > 0 && elementInfo.xpos < 19
+    )
+  }
+
+  const CellIsPeriodNr = () => {
+    return (
+      elementInfo.xpos === 0 && elementInfo.ypos > 0 && elementInfo.ypos < 8
+    )
+  }
 
   if (elementInfo.name) {
     const categoryClass = elementInfo.category.substring(0, 7).trim()
     return (
-      <div
-        className={`Cell elmtCell ${categoryClass}`}
-        // onMouseEnter={handleMouseEnter}
-        onClick={handleClick}
-      >
+      <div className={`Cell elmtCell ${categoryClass}`} onClick={handleClick}>
         <div className='elmtNumber'>{elementInfo.number}</div>
         <div className='elmtSymbol'>{elementInfo.symbol}</div>
         <div className='elmtName'>{elementInfo.name}</div>
         <div className='elmtInfo'>{elementInfo.atomic_mass.toFixed(2)}</div>
       </div>
     )
-  } else if (
-    elementInfo.ypos === 0 &&
-    elementInfo.xpos > 0 &&
-    elementInfo.xpos < 19
-  ) {
-    return <div className='Cell periodOrGroupCell'>{elementInfo.xpos}</div>
-  } else if (
-    elementInfo.xpos === 0 &&
-    elementInfo.ypos > 0 &&
-    elementInfo.ypos < 8
-  ) {
-    return <div className='Cell periodOrGroupCell'>{elementInfo.ypos}</div>
+  } else if (CellIsGroupNr()) {
+    return <PeriodGroupCell props={elementInfo.xpos} />
+  } else if (CellIsPeriodNr()) {
+    return <PeriodGroupCell props={elementInfo.ypos} />
   } else return <div className='Cell emptyCell'></div>
 }
 
---
+++ src/components/PeriodGroupCell/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./index.css"
+
+const PeriodGroupCell = ({ props }) => {
+  return <div className='Cell PeriodGroupCell'>{props}</div>
+}
+
+export default PeriodGroupCell
