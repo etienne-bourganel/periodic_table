4475e1bb95ace1ba9bdf30c90c9ee5bbe6c4ca10
Etienne Bourganel <etienne.bourganel@gmail.com>
2021-06-24 10:11:26 +0200

    Rename component and simplify highlight function

--- src/components/ElementCell/index.js
+++ src/components/ElementCell/index.js
@@ -22,14 +22,12 @@ const ElementCell = ({
   const categoryClass = elementInfo.category.substring(0, 7).trim()
 
   let highlight = () => {
-    if (
+    return (
       PeriodOrGroupToHighlight.xpos === elementInfo.xpos ||
       PeriodOrGroupToHighlight.ypos === elementInfo.ypos ||
       (PeriodOrGroupToHighlight.ypos === 6) & (elementInfo.ypos === 9) ||
       (PeriodOrGroupToHighlight.ypos === 7) & (elementInfo.ypos === 10)
-    ) {
-      return true
-    }
+    )
   }
 
   return (
