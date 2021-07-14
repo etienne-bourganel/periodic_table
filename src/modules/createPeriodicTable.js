8aa248fdb97c9f506dc01d3b6b4f002893ed94e7
Etienne Bourganel <etienne.bourganel@gmail.com>
2021-07-14 07:54:13 +0200

    Fix layout issue table height

--- src/modules/createPeriodicTable.js
+++ src/modules/createPeriodicTable.js
@@ -1,12 +1,12 @@
-const periodicTable = require("../data/data.json")
-const periods_groups = require("../data/periods_groups.json")
+const periodicTable = require("../data/data.json");
+const periods_groups = require("../data/periods_groups.json");
 
 const createPeriodicTable = () => {
-  let emptyTable = []
+  let emptyTable = [];
 
   for (let ypos = 0; ypos <= 10; ypos++) {
-    for (let xpos = 0; xpos <= 19; xpos++) {
-      emptyTable.push({ xpos, ypos })
+    for (let xpos = 0; xpos <= 18; xpos++) {
+      emptyTable.push({ xpos, ypos });
     }
   }
 
@@ -15,7 +15,7 @@ const createPeriodicTable = () => {
       periodicTable.elements.find(
         (element) => element.xpos === cell.xpos && element.ypos === cell.ypos
       ) || { ...cell, data: null }
-  )
+  );
 
   const newTableTemplate = tableTemplate.map(
     (cell) =>
@@ -23,9 +23,9 @@ const createPeriodicTable = () => {
         (periodOrGroup) =>
           periodOrGroup.xpos === cell.xpos && periodOrGroup.ypos === cell.ypos
       ) || { ...cell, data: null }
-  )
+  );
 
-  return newTableTemplate
-}
+  return newTableTemplate;
+};
 
-export default createPeriodicTable
+export default createPeriodicTable;
